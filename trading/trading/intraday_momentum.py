7b5f4d0e6e644819fc133a3bc1c41d2cd2678d0a
pranesh <praneshbalekai@gmail.com>
2025-01-10 01:22:30 +0800

    add volume real-time data

--- trading/trading/intraday_momentum.py
+++ trading/trading/intraday_momentum.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import argparse
+import datetime
+import json
 import threading
 import time
 from decimal import Decimal
@@ -9,31 +12,30 @@ import schedule
 from ibapi.contract import Contract
 
 import external.ibkr as ibkr
+from cio.data_loader import load_data
+from core.strategy import load_noise_area
 
-# from core.strategy import load_noise_area
-
-# Pre run, load yesterday's data and keep ready
-
-# TODO:
-# Subscribe to 5s bars, consolidate it to 30 min bars
-
-# Find a way to check entry / exit conditions at the completion of a 30 min bar
-# Rem, position sizing, sending orders
-
-# Strategy defaults
-lookback_days = 20
-volatility_multiplier = 1
-vol_scaling_target = 0.02
+parser = argparse.ArgumentParser(description="Path of config file to pass to script")
+parser.add_argument("-c", "--config_path", type=str, help="Path to config file")
 
 
 class IntradayMomentum(ibkr.IBBaseApp):
     def __init__(self, config: dict):
         super().__init__()
-        self.live_data = pd.DataFrame()
+        self.current_limits = None
+        self.current_open = None
+        self.live_data = pd.DataFrame()  # used by 5 min bars
+        self.mins = {}  # used for tick by tick data
         self.config = config
-        self.number_of_bars = 1
+        self.number_of_bars = 1  # used by 5 min bars
+        (
+            self.historical_data,
+            self.latest_avg,
+            self.last_close,
+        ) = self.init_historical_data_to_strategy()
 
     # We might not use this until we have a live data subscription
+    # TODO: Assume incomplete until we have paid for subscription and tested
     def is_complete_bar(self, data: pd.DataFrame):
         """Re-sample 5s live data dataframe and returns True, if it is a complete bar at target freq
 
@@ -76,8 +78,6 @@ class IntradayMomentum(ibkr.IBBaseApp):
         count: int,
     ):
         """Handler function to receive 5s bars. This function, will make 30min bars from it."""
-        print(time)
-        print(open_)
         bar = {}
         data = {
             "close": close,
@@ -94,39 +94,144 @@ class IntradayMomentum(ibkr.IBBaseApp):
     def marketDataType(self, reqId: int, marketDataType: int):
         print("MarketDataType. ReqId:", reqId, "Type:", marketDataType)
 
-    # TODO: Convert ticks into bars. For now, we get delayed data,
-    # so the bars will NOT reflect real-time / historical values.
-    # This is just to get into paper trading. Will need to pay for live subscriptions.
     def tickPrice(self, reqId, tickType, price, attrib):
-        # append to a kv store with incoming time and ltp (tick ID, 68?)
-        print(reqId, tickType, price, attrib)
+        # TODO: Change this to real time last price once we switch to paid subscription.
+        # type 68 is delayed last price
+        if tickType == 68:
+            dt = datetime.datetime.now(datetime.timezone.utc)
+            dt = dt.replace(second=0, microsecond=0)
+
+            if self.current_open is None:
+                self.current_open = price
+                self.current_limits = self.load_strategy_limits()
+            # TODO: is there a defaultdict that does this?
+            if dt in self.mins:
+                self.mins[dt]["close"] = price
+                if "open" not in self.mins[dt]:
+                    self.mins[dt]["open"] = price
+                if "low" not in self.mins[dt] or self.mins[dt]["low"] > price:
+                    self.mins[dt]["low"] = price
+                if "high" not in self.mins[dt] or self.mins[dt]["high"] < price:
+                    self.mins[dt]["high"] = price
+            elif dt not in self.mins:
+                self.mins[dt] = {}
+                self.mins[dt]["open"] = price
+                self.mins[dt]["close"] = price
+                self.mins[dt]["low"] = price
+                self.mins[dt]["high"] = price
+    
+    def tickSize(self, reqId, tickType, size):
+        # tick type 71, delayed last size
+        if tickType == 71:
+            dt = datetime.datetime.now(datetime.timezone.utc)
+            dt = dt.replace(second=0, microsecond=0)
+
+            if dt in self.mins:
+                if 'volume' in self.mins[dt]:
+                    self.mins[dt]['volume'] += size
+                else:
+                    self.mins[dt]['volume'] = size
+            elif dt not in self.mins:
+                self.mins[dt] = {}
+                self.mins[dt]['volume'] = size
+
+    def load_strategy_limits(self):
+        if self.current_limits is None:
+            limits = self.latest_avg.copy()
+            limits["upper_limit"] = max(self.last_close, self.current_open) * (
+                1 + (self.config["strategy"]["volatility_multiplier"] * self.latest_avg)
+            )
+            limits["lower_limit"] = min(self.last_close, self.current_open) * (
+                1 - (self.config["strategy"]["volatility_multiplier"] * self.latest_avg)
+            )
+            return limits
 
     def run_strategy(self):
-        pass
+        df = pd.DataFrame().from_dict(self.mins, orient="index")
+        # TODO: see how to get TZ from config
+        df.index = pd.Series(df.index).dt.tz_convert("US/Eastern")
+
+        # At this point, index type and tz should be aligned
+        df = df.resample("30min").agg(
+            open=("open", "first"),
+            close=("close", "last"),
+            high=("high", "max"),
+            low=("low", "min"),
+            volume=("volume", "sum")
+        )
+
+    def init_historical_data_to_strategy(self):
+        """Loads historical data and manipulate as required for the strategy."""
+        df = load_data(self.config["historical_data"])
+        df, latest_avg = load_noise_area(
+            df,
+            lookback_days=self.config["strategy"]["lookback_days"],
+            volatility_multiplier=self.config["strategy"]["volatility_multiplier"],
+        )
+        last_close = df.iloc[-1]["day_close"]
+        return df, latest_avg.to_frame(), last_close
 
 
-app = IntradayMomentum({})
-app.connect("127.0.0.1", 4002, clientId=1)
-
-threading.Thread(target=app.run).start()
-time.sleep(1)
-
-contract = Contract()
-contract.symbol = "SPY"
-contract.secType = "STK"
-contract.currency = "USD"
-contract.exchange = "SMART"
-
-# Gotta start paper trading with delayed data
-app.reqMarketDataType(3)
-app.reqMktData(app.nextId(), contract, "", False, False, [])
-
-
-# New thread - run this in a new thread
-def custom_sched():
-    schedule.every().hour.at(":30").do(app.run_strategy)
+def custom_sched(app: IntradayMomentum):
+    """Scheduler to start, run and end strategy."""
+    schedule.every(1).minutes.do(app.run_strategy)
     while True:
         # Checks whether a scheduled task
         # is pending to run or not
         schedule.run_pending()
         time.sleep(1)
+
+
+def main(config_path: str):
+    """
+    Example Config:
+    {
+        "historical_data": {
+            "loader_class": "ParquetLoader",
+            "filename": "/Users/praneshbalekai/Desktop/IB_PRD/data/spy_mins.parquet"
+        },
+        "strategy": {
+            "lookback_days": 20,
+            "volatility_multiplier": 0.8
+        },
+        "ibkr_params": {
+            "genericTickList": "",
+            "snapshot": false,
+            "regulatorySnapshot": false,
+            "chartOptions": []
+        },
+        "contract": {
+            "symbol": "SPY",
+            "secType": "STK",
+            "exchange": "SMART",
+            "currency": "USD"
+        }
+    }
+    """
+    config = open(config_path)
+    config = json.load(config)
+
+    app = IntradayMomentum(config)
+    app.connect("127.0.0.1", 4002, clientId=1)
+
+    threading.Thread(target=app.run).start()
+    time.sleep(1)
+    threading.Thread(target=custom_sched, args=(app,)).start()
+    time.sleep(1)
+
+    ibkr_params = config["ibkr_params"]
+    ibkr_params["reqId"] = app.nextId()
+    ibkr_params["contract"] = Contract()
+    for k, v in config["contract"].items():
+        setattr(ibkr_params["contract"], k, v)
+
+    # Gotta start paper trading with delayed data
+    app.reqMarketDataType(3)
+    app.reqMktData(**ibkr_params)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(f"Input args: {args.__dict__}")
+
+    main(args.config_path)
